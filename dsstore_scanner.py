72e43ce05706adbb8add313e3aeda06f210e5b80
Xavier Garceau-Aranda <xavier.garceau-aranda@owasp.org>
2017-08-09 10:47:39 +0200

    Test passive scan

--- dsstore_scanner.py
+++ dsstore_scanner.py
@@ -95,27 +95,27 @@ class BurpExtender(IBurpExtender, IScannerCheck, IExtensionStateListener):
         request = self._requestResponse.getRequest()
         filename = request.tostring().split()[1]
 
-        if filename is ".DS_Store":
-            host = self._requestResponse.getHttpService().getHost()
-            response = self._requestResponse.getResponse()
-            responseInfo = self._helpers.analyzeResponse(response)
-            bodyOffset = responseInfo.getBodyOffset()
+        # if filename is ".DS_Store":
+        host = self._requestResponse.getHttpService().getHost()
+        response = self._requestResponse.getResponse()
+        responseInfo = self._helpers.analyzeResponse(response)
+        bodyOffset = responseInfo.getBodyOffset()
 
-            issuename = "Found .DS_Store file"
-            issuelevel = "Low"
-            issuedetail = """<p>Found .DS_Store file.</p>"""
-            log = "[+] .DS_Store: " + host + "\n"
-            issueremediation = """Some remediation"""
+        issuename = "Found .DS_Store file"
+        issuelevel = "Low"
+        issuedetail = """<p>Found .DS_Store file.</p>"""
+        log = "[+] .DS_Store: " + host + "\n"
+        issueremediation = """Some remediation"""
 
-            # Create a ScanIssue object and append it to our list of issues
-            self.scan_issues.append(ScanIssue(self._requestResponse.getHttpService(),
-                                              self._helpers.analyzeRequest(
-                                                  self._requestResponse).getUrl(),
-                                              issuename,
-                                              issuelevel,
-                                              issuedetail,
-                                              issueremediation))
-            self.logsTA.append(log)
+        # Create a ScanIssue object and append it to our list of issues
+        self.scan_issues.append(ScanIssue(self._requestResponse.getHttpService(),
+                                          self._helpers.analyzeRequest(
+                                              self._requestResponse).getUrl(),
+                                          issuename,
+                                          issuelevel,
+                                          issuedetail,
+                                          issueremediation))
+        self.logsTA.append(log)
 
     def consolidateDuplicateIssues(self, existingIssue, newIssue):
         if existingIssue.getUrl() == newIssue.getUrl() and \
